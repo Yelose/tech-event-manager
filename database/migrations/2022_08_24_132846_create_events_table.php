00b2a65fe3d2621b5e1ef955f6d0327b243b3f93
Estherherrera <graxyherrera@gmail.com>
2022-08-25 12:48:56 +0200

    base de datos

--- database/migrations/2022_08_24_132846_create_events_table.php
+++ database/migrations/2022_08_24_132846_create_events_table.php
@@ -15,6 +15,15 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->date('fecha');
+            $table->time('Hour');
+            $table->string('duration');
+            $table->integer('max_participants');
+            $table->integer('min_participants');
+            $table->integer('price');
+            $table->string('description', 1000);
+            $table->string('Included', 1000);
             $table->timestamps();
         });
     }

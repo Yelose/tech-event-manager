77ac52eb8d08dde187890cb18309b3cf7a7a0737
Marina González Suárez <yelose85@gmail.com>
2022-08-26 08:33:29 +0200

    new branches

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,19 +10,65 @@
 
 <body>
     @if (Route::has('login'))
-    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-        @auth
-        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-        @else
-        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+    <div class="hidden navbar fixed top-0 right-0 px-6 py-4 sm:block">
+        <img src="{{ asset('../img/logoexcursiones.png') }}" alt="logo">
+        <div class="navigation-menu">
+            <a href="{{ url('/') }}">Inicio</a>
+            @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+            @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-        @if (Route::has('register'))
-        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-        @endif
-        @endauth
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+            @endauth
+        </div>
     </div>
     @endif
-
+    <div class="slider"></div>
 </body>
 
 </html>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .slider {
+        width: 100%;
+        height: 40vh;
+        background-image: url("https://live.staticflickr.com/7750/17906978882_7d028d8daa_b.jpg");
+        background-position: top;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    .navbar {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #712E3D;
+        padding: 10px;
+    }
+
+    .navigation-menu>a {
+        padding: 10px;
+        text-decoration: none;
+        color: white;
+        font-size: clamp(14px, 2vw, 20px);
+    }
+
+    .navbar>img {
+        width: 50px;
+        height: 50px;
+    }
+</style>

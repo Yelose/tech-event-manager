bf4efb0b4dee69d7d5f205a061b64b50bd11c78c
Estherherrera <graxyherrera@gmail.com>
2022-08-29 10:05:03 +0200

    agregando layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.main')
+@section('content')
+    
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <title>Excursiones Fem-Desk</title>
-</head>
-
-<body>
     @if (Route::has('login'))
     <div class="hidden navbar fixed top-0 right-0 px-6 py-4 sm:block">
         <img src="{{ asset('../img/logoexcursiones.png') }}" alt="logo">
@@ -53,12 +45,9 @@
     @endempty
     {{-- <p>{{$events}}</p> --}}
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+@endsection 
    
-</body>
 
-</html>
 <style>
     * {
         margin: 0;

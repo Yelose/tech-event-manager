b2f7b0876b495104277c357a224c79978c876179
Estherherrera <graxyherrera@gmail.com>
2022-09-07 10:26:40 +0200

    preparando el mergeo

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Event;
 
 Route::get('/', [EventController::class, 'home'])->name('home');
-Route::get('/show/{event}', ['events' => Event::orderBy('created_at', 'desc')->get()]);
-// Route::get('/show/{event}', [EventController::class, 'show']);
+Route::get('/show/{event}', [EventController::class, 'show']);
 Route::get('/edit', [EventController::class, 'edit']);
 Route::get('/create', [EventController::class, 'create'])->name('create');
 Route::post('/', [EventController::class, 'store'])->name('store');
 
 
 Route::get('/dashboard', function () {
+    $events =Event::all();
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
